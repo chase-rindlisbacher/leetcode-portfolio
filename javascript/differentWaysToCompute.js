8e68c31e2aa3e94c130999e2534c6a7dfad999d5
chase-rindlisbacher <103144954+chase-rindlisbacher@users.noreply.github.com>
2026-03-09 16:36:10 -0600

    this is my working commented version

--- javascript/differentWaysToCompute.js
+++ javascript/differentWaysToCompute.js
@@ -1,9 +1,15 @@
+// Leetcode Problem 241
+
+/**
+ * @param {string} expression
+ * @return {number[]}
+ */
 var diffWaysToCompute = function(expression) {
-    if (!isNaN(expression)) {
+    if (!isNaN(expression)) { // if the input is just a number, return it as an array of Numbers
         return [Number(expression)]
     }
     const returnArray = []
-    const compute = (operand1, operand2, operator) => {
+    const compute = (operand1, operand2, operator) => { // take 2 numbers and an operator then perform the computation and return the result
         if (operator === "*") {
             return Number(operand1) * Number(operand2);
         }
@@ -17,19 +23,19 @@ var diffWaysToCompute = function(expression) {
     for (let i = 0; i < expression.length; i++) {
         let char = expression[i]
         if (char === "+" || char === "-" || char === "*") {
-            let leftResults = diffWaysToCompute(expression.slice(0,i))
-            let rightResults = diffWaysToCompute(expression.slice(i + 1,expression.length + 1))
+            let leftResults = diffWaysToCompute(expression.slice(0,i)) // Take the left side of this operator and recursively call this function again with it
+            let rightResults = diffWaysToCompute(expression.slice(i+1)) // Take the right side of this operator and recursively call this function with it
             
-            for (const left of leftResults) {
+            for (const left of leftResults) { 
                 for (const right of rightResults) {
-                    returnArray.push(compute(left,right,char))
+                    returnArray.push(compute(left,right,char)) // Compute the outcome of the small expression and add it to the return array
                 }
             }
         }
 
     }
 
-    return returnArray
+    return returnArray // Return the array of computed values
 };
 
 
